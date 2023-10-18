f4b5b557cc322319f69254b97a6d3f09eca3c9c3
ForsanNemah <forsan20172017@gmail.com>
2023-10-18 04:54:58 +0300

    add

--- app/Http/Controllers/filecontroller.php
+++ app/Http/Controllers/filecontroller.php
@@ -14,22 +14,40 @@ class filecontroller extends Controller
 
 //echo $re->quality;
 
+
         
         $data['file']=$re->file('file')->store('apifiles',['disk' => 'public']);
 
 
-        Vila::create($data);
+
+
+        Vila::create(
+
+
+            array(
+                'quality' => $re->quality."/5",
+                'file'  => $data['file'],
+                'branch1' => $re->branch1,
+
+                'sellers_satis'  => $re->sellers_satis."/5",
+                'cashier_satis'  => $re->cashier_satis."/5",
+                'total_satis'  => $re->total_satis."/5",
+                'note'  => $re->note,
+            )
+
+
+        );
 
         echo '<script type="text/javascript">
         
         alert("تمت  العملية بنجاح          ");
-        window.open("http://pscye.com/kaif/vla/", "_self");
-        
+        //window.open("http://pscye.com/kaif/vla/", "_self");
+        window.history.go(-1);
         </script>
         
         ';
      
-
-        //return  $result;
+        
+         
     }
 }
